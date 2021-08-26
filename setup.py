a110c47db21fdb6540de2b54879db63c34f748db
pbandj082 <pbandj082@gmail.com>
2021-08-26 22:15:22 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 def _get_requires():
     with open('requirements.txt') as f:
-        return f.readlines()
+        return [p.rstrip() for p in f.readlines()]
 
 setup(
     name='logen',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/pbandj082/logen',
     packages=['logen', 'logen.adapters'],
     requires=_get_requires(),
